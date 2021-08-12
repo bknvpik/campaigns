ce1ca9f3c30b49fae68d21c7040151a01010a208
fvzzini <b.knvpik@gmail.com>
2021-08-12 18:21:53 +0200

    Add firebase data to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA108kk6ASbLAnh_p2DKVb-DbCtaV2o6bw",
+    authDomain: "campaigns-4fea9.firebaseapp.com",
+    projectId: "campaigns-4fea9",
+    storageBucket: "campaigns-4fea9.appspot.com",
+    messagingSenderId: "620702446219",
+    appId: "1:620702446219:web:577bf5e1a33ac33ec7da8e",
+    measurementId: "G-5FJBMDVJB1"
+  }
 };
